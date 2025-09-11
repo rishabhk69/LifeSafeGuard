feae7e6d021a9f37791a3f0cf692fc81bf47eac3
HP <rishabhkhangarot1999@gmail.com>
2025-09-11 12:08:04 +0530

    updated code 11-09-2025

---
+++ lib/screens/auth_screens/choose_login.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:untitled/constants/common_background.dart';
+import 'package:untitled/constants/custom_button.dart';
+import 'package:untitled/constants/image_helper.dart';
+import 'package:untitled/constants/strings.dart';
+
+class ChooseLogin extends StatefulWidget {
+  const ChooseLogin({super.key});
+
+  @override
+  State<ChooseLogin> createState() => _ChooseLoginState();
+}
+
+class _ChooseLoginState extends State<ChooseLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return CommonBackground(
+        iconName: ImageHelper.startHereIc,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(),
+           Column(
+             children: [
+               CustomButton(
+                   buttonHeight: 50,
+                   text: StringHelper.login, onTap: (){
+                 context.go('/mobileScreen',extra: {
+                   'isLogin': 'true',
+                 });
+               }),
+               Row(
+                 children: [
+                   Expanded(child: Divider()),
+                   Text('Or'),
+                   Expanded(child: Divider()),
+                 ],
+               ),
+               CustomButton(
+                   buttonHeight: 50,
+                   text: StringHelper.signUP, onTap: (){
+                 context.go('/mobileScreen',extra: {
+                   'isLogin': 'false',
+                 });
+               }),
+             ],
+           ),
+            SizedBox(),
+            SizedBox(),
+          ],
+        ));
+  }
+}
