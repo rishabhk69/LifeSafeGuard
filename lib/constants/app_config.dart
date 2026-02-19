c3c83fd3e7421785e470af75713fb580335386f9
HP <rishabhkhangarot1999@gmail.com>
2026-02-19 17:47:06 +0530

    updated code 19-02-2026

--- lib/constants/app_config.dart
+++ lib/constants/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   //Production
   // static const HOST = 'http://192.168.29.254:5000/api';
-  static const HOST = 'https://protector.lifesafeguard.in/api/v1/';
+  static const HOST = 'https://test.lifesafeguard.in/api/v1/';
   static String IMAGE_BASE_URL = 'https://lifesafeguardpictures.s3.ap-south-1.amazonaws.com/';
   static String VIDEO_BASE_URL = 'https://lifesafeguardvideos.s3.ap-south-1.amazonaws.com/';
 
